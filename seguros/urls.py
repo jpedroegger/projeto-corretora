1b81f9766161669c1b024c6e81ecf00e0f9fec86
jpedroegger <jpedroegger@gmail.com>
2022-06-01 01:06:24 +0100

    url refactor

--- seguros/urls.py
+++ seguros/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('lista_segurados/', views.lista_segurados, name='lista_segurados'),
 
-    #path('novo_cadastro/<int:pk>', views.novo_cadastro, name='novo_cadastro'),
-
     path('nova_apolice/<int:pk>', views.nova_apolice, name='nova_apolice'),
     path('novo_segurado/', views.novo_segurado, name='novo_segurado'),
 
@@ -15,7 +13,7 @@ urlpatterns = [
     path('segurado/<int:pk>', views.ver_segurado, name='ver_segurado'),
 
     path('edit_apolice/<str:pk>', views.editar_apolice, name='editar_apolice'),
-    path('edit_segurado/<str:pk>', views.editar_segurado, name='editar_segurado'),
+    path('edit_segurado/<int:pk>', views.editar_segurado, name='editar_segurado'),
     
     path('del_segurado/<int:pk>', views.deletar_segurado, name='deletar_segurado'),
     path('del_apolice/<str:pk>', views.deletar_apolice, name='deletar_apolice'),    
